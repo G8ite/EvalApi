92eb7b1760495e0e5048e088aa25f3fff84323aa
Amandine Pimentel de Andrade <amandine@spirkop.com>
2024-06-11 20:11:12 +0200

    nouvel essai corection probleme position

--- server.js
+++ server.js
@@ -42,9 +42,10 @@ wss.on('connection', ws => {
 
                 // Envoie la géolocalisation à tous les autres clients
                 wss.clients.forEach(client => {
-                    if (client !== ws && client.readyState === WebSocket.OPEN) {
+                    if (client.readyState === WebSocket.OPEN) {
                         client.send(JSON.stringify({
                             type: 'geolocation',
+                            id: ws._socket.remoteAddress, // Utiliser l'adresse IP comme identifiant unique
                             payload: { latitude, longitude }
                         }));
                     }
