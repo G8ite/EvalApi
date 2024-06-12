8b38622251a0c090e0cec571a324f8f10bc84e74
Amandine Pimentel de Andrade <amandine@spirkop.com>
2024-06-12 16:51:11 +0200

    changement de stun servers

--- public/client.js
+++ public/client.js
@@ -132,12 +132,18 @@ const startCall = (room, otherUserId) => {
             // Création de la connexion peer-to-peer
             const peerConnection = new RTCPeerConnection({
                 iceServers: [
-                    { urls: 'stun:stun.l.google.com:19302' },
-                    { urls: 'stun:stun1.l.google.com:19302' },
-                    { urls: 'stun:stun2.l.google.com:19302' },
-                    { urls: 'stun:stun3.l.google.com:19302' },
-                    { urls: 'stun:stun4.l.google.com:19302' }
+                    { urls: "stun:stun.l.google.com:19302" },
+                    { urls: "stun:stun.l.google.com:5349" },
+                    { urls: "stun:stun1.l.google.com:3478" },
+                    { urls: "stun:stun1.l.google.com:5349" },
+                    { urls: "stun:stun2.l.google.com:19302" },
+                    { urls: "stun:stun2.l.google.com:5349" },
+                    { urls: "stun:stun3.l.google.com:3478" },
+                    { urls: "stun:stun3.l.google.com:5349" },
+                    { urls: "stun:stun4.l.google.com:19302" },
+                    { urls: "stun:stun4.l.google.com:5349" }
                 ]
+
             });
             peerConnections[room] = peerConnection;
 
