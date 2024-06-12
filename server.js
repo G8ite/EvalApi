538aca02250c0f5e9690ef8b3d7908a1224ce9d2
Amandine Pimentel de Andrade <amandine@spirkop.com>
2024-06-12 12:04:44 +0200

    7eme essai appel visio

--- server.js
+++ server.js
@@ -31,21 +31,17 @@ io.on('connection', socket => {
     });
 
     socket.on('requestChat', targetUserId => {
-        io.to(targetUserId).emit('showModal', socket.id);
+        io.to(targetUserId).emit('chatRequested', socket.id);
     });
 
     socket.on('userClicked', targetUserId => {
         io.to(targetUserId).emit('showModal', socket.id);
     });
 
-    socket.on('acceptChat', (userIds) => {
-        userIds.forEach(userId => {
-            if (userId !== socket.id) { // Évitez de créer une connexion avec soi-même
-                const newConnection = createPeerConnection(userId);
-                connections.set(userId, newConnection);
-            }
-        });
-        io.emit('startCall', userIds); // Émet un événement pour démarrer l'appel avec les utilisateurs concernés
+    socket.on('acceptChat', (requesterId) => {
+        io.to(requesterId).emit('chatAccepted', socket.id);
+        io.to(socket.id).emit('chatAccepted', requesterId);
+        io.emit('showModal', [requesterId, socket.id]);
     });
 
     socket.on('webrtcSignal', (data) => {
@@ -53,20 +49,6 @@ io.on('connection', socket => {
         io.to(target).emit('webrtcSignal', { signal, from: socket.id });
     });
 });
-
-function createPeerConnection(userId) {
-    const peerConnection = new RTCPeerConnection(iceServers);
-    peerConnection.onicecandidate = event => {
-        if (event.candidate) {
-            io.to(userId).emit('webrtcSignal', { target: userId, signal: event.candidate });
-        }
-    };
-    peerConnection.ontrack = event => {
-        // Envoyer le flux vidéo reçu à l'utilisateur
-        io.to(userId).emit('remoteStream', { stream: event.streams[0], userId: socket.id });
-    };
-    return peerConnection;
-}
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
